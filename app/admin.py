5f5aba257a15efb3159b1d84dbb9355b3396e4ae
Apolexian <nikitin.ivan.dev@gmail.com>
2020-03-04 21:35:39 +0000

    Aletered admin view
    Also fixed the save method on category to actually save

--- app/admin.py
+++ app/admin.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
-from app.models import Category, UserProfile
+from app.models import Category, UserProfile, Rating, Wishlist, Product, \
+    SubCategory
 
 
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
+    list_display = ('name', 'description')
 
 
+admin.site.register(Rating)
+admin.site.register(SubCategory)
+admin.site.register(Wishlist)
+admin.site.register(Product)
 admin.site.register(UserProfile)
 admin.site.register(Category, CategoryAdmin)
 
