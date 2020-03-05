6ea88b46514172ccd874d0fb246cd1924911097e
Apolexian <nikitin.ivan.dev@gmail.com>
2020-03-05 21:00:30 +0000

    profiles now save with users

--- app/views.py
+++ app/views.py
@@ -67,13 +67,14 @@ def register(request):
     registered = False
     if request.method == "POST":
         user_form = UserForm(request.POST)
-        profile_form = UserProfileForm(request.POST)
+        profile_form = UserProfileForm(request.POST, request.FILES)
         if (user_form.is_valid() and profile_form.is_valid()):
             user = user_form.save()
             user.set_password(user.password)
             user.save()
             profile = profile_form.save(commit=False)
             profile.user = user
+            profile.save()
             if "picture" in request.FILES:
                 profile.picture = request.FILES["picture"]
             profile.save()
