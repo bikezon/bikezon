85e908d273c7fdc12bbdc62b1dc1f50ac0cfa085
Magdalena Latifa <2398248l@student.gla.ac.uk>
2020-03-14 22:58:24 +0000

    added hosting domain to allowed hosts

--- bikezon/settings.py
+++ bikezon/settings.py
@@ -44,7 +44,7 @@ SECRET_KEY = 'tqbagvz-mhdh2##rop@(2g$_na^*g07m-tor3mnh8%e#=+w+=r'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1','bikezon.pythonanywhere.com']
 
 
 # Application definition
