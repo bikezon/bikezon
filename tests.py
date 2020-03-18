dcbd4d20ec482ca2d2cdab61d33e6745a0c43bb4
Magdalena Latifa <2398248l@student.gla.ac.uk>
2020-03-18 16:09:04 +0000

    tests for redirects on base html

--- tests.py
+++ tests.py
@@ -7,9 +7,6 @@ import logging
 from app.models import UserProfile, User, ProductList
 from app.forms import UserForm
 import unittest
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium import webdriver
 from selenium import common
 
@@ -136,6 +133,68 @@ class TestLogIn(unittest.TestCase):
         self.tearDown()
 
 
+class TestBaseRedirects(unittest.TestCase):
+
+    def __init__(self):
+        self.driver = webdriver.Firefox()
+
+
+    def test_base_redirects(self):
+        self.driver.get("http://127.0.0.1:8000/")
+        # check login button takes user to login
+        self.driver.find_element_by_id('id_login').click()
+        if "http://127.0.0.1:8000/login/" == self.driver.current_url:
+            test_logger.info("Correct redirect to sign in on base")
+        else:
+            test_logger.warning("Wrong redirect to sign in on base")
+
+        # go back to main page and try to log out or login and and logout
+        self.driver.get("http://127.0.0.1:8000/")
+        try:
+            self.driver.find_element_by_id('id_logout').click()
+        except common.exceptions.NoSuchElementException as e:
+            # if cant logut then login and logout
+            self.driver.find_element_by_id('id_login').click()
+            self.driver.find_element_by_id('id_username').send_keys('TestUser')
+            self.driver.find_element_by_id('id_password').send_keys('1234')
+            self.driver.find_element_by_id('log-in-button').click()
+            self.driver.get("http://127.0.0.1:8000")
+            self.driver.find_element_by_id('id_logout').click()
+
+        # check that log redirects to index
+        if "http://127.0.0.1:8000/" == self.driver.current_url:
+            test_logger.info("Correct redirect from logout")
+        else:
+            test_logger.warning("Wrong redirect from logout")
+        
+        # redirect back to home page
+        self.driver.get("http://127.0.0.1:8000/")
+
+        # login as test user
+        self.driver.find_element_by_id('id_login').click()
+        self.driver.find_element_by_id('id_username').send_keys('TestUser')
+        self.driver.find_element_by_id('id_password').send_keys('1234')
+        self.driver.find_element_by_id('log-in-button').click()
+
+        # go to test user's account to check redirect
+        self.driver.find_element_by_id('id_account').click()
+        if "http://127.0.0.1:8000/account/" == self.driver.current_url:
+            test_logger.info("Correct redirect to account from base")
+        else:
+            test_logger.warning("Wrong redirect to account from base")
+        
+            
+    
+    # kill driver on teardown
+    def tearDown(self):
+        self.driver.quit()
+
+    # setup driver, run the tests and kill the driver
+    def run(self):
+        self.test_base_redirects()
+        self.tearDown()
+
+
 # launch tests
 if __name__ == '__main__':
     print("Starting all tests, this may take a while...")
@@ -153,3 +212,5 @@ if __name__ == '__main__':
     login_tester.run()
     print("Ok.")
     test_logger.info("All passed.")
+    redirect_tester = TestBaseRedirects()
+    redirect_tester.run()
