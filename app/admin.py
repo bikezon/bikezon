66b6e662deadff2428d608ada646c47923399ea0
Magdalena Latifa <2398248l@student.gla.ac.uk>
2020-03-30 10:48:23 +0100

    started working on admin panel

--- app/admin.py
+++ app/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from app.models import Category, UserProfile, Rating, ProductList, Product, \
     SubCategory
 
+
 class CategoryAdmin(admin.ModelAdmin):
     """admin view for categories, displays
     the name and description of the category
@@ -10,10 +11,29 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'description')
 
 
+class ProductAdmin(admin.ModelAdmin):
+    """admin view for products, prepopulates slugs,
+    displays name, description, date added, price
+    and the seller
+    """
+    prepopulated_fields = {'slug': ('name',)}
+    list_display = ('name', 'description', 'dateAdded', 'price', 'seller')
+
+
+class ProductListAdmin(admin.ModelAdmin):
+    """admin view for product lists (e.g wish list),
+    prepopulates slug, displays the name, the user
+    to whom the list belongs and the products on the
+    list.
+    """
+    prepopulated_fields = {'slug': ('name',)}
+    list_display = ('name', 'user', 'product')
+
+
 admin.site.register(Rating)
 admin.site.register(SubCategory)
-admin.site.register(ProductList)
-admin.site.register(Product)
+admin.site.register(ProductList, ProductListAdmin)
+admin.site.register(Product, ProductAdmin)
 admin.site.register(UserProfile)
 admin.site.register(Category, CategoryAdmin)
 
